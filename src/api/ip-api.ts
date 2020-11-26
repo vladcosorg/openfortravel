0347c2034848198a3a3e07fa5395051967f0a2c7
chetzof <vcosvic@gmail.com>
2020-11-26 15:50:06 +0200

    Fixed

--- src/api/ip-api.ts
+++ src/api/ip-api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export async function fetchCurrentCountryCode(): Promise<string> {
-  const response = await axios.get<{ countryCode: string }>(
-    '//ip-api.com/json/',
+  const response = await axios.get<{ country_code: string }>(
+    'https://api.ipdata.co?api-key=d7e6b4605424e5ae000386de7ab7408394c12b8395408051fbcaec80',
   )
-  return response.data.countryCode.toLowerCase()
+  return response.data['country_code'].toLowerCase()
 }
