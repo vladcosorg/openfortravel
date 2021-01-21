244c0560a2f698d2b38888f76125f7dae9a54e30
chetzof <vcosvic@gmail.com>
2021-01-21 19:10:24 +0200

    fix:  fix

---
+++ packages/front/src/pages/country/components/input-filter.vue
@@ -0,0 +1,50 @@
+<template>
+  <q-input
+    :value="value"
+    :placeholder="$t('page.country.quickSearch')"
+    :loading="isLoading"
+    standout
+    dense
+    debounce="300"
+    stack-label
+    dark
+    v-on="$listeners"
+  >
+    <template #prepend>
+      <q-icon :name="iconSearch" />
+    </template>
+    <template #after>
+      <subscribe-button :origin-code="originCode" />
+    </template>
+  </q-input>
+</template>
+
+<style lang="scss" module></style>
+
+<script lang="ts">
+import { matSearch as iconSearch } from '@quasar/extras/material-icons'
+import { defineComponent } from '@vue/composition-api'
+
+import SubscribeButton from '@/front/src/pages/country/components/subscribe-button.vue'
+
+export default defineComponent({
+  components: { SubscribeButton },
+  props: {
+    originCode: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+    isLoading: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  setup(props) {
+    return { iconSearch }
+  },
+})
+</script>
--- packages/front/src/pages/country/components/search-results.vue
+++ packages/front/src/pages/country/components/search-results.vue
@@ -12,23 +12,11 @@
         {{ $t('page.country.destinations') }}
       </div>
       <search-tabs v-model="tabFilterValue" :origin-code="originCode" />
-      <q-input
+      <input-filter
         v-model="countryMatchFilterValue"
-        :placeholder="$t('page.country.quickSearch')"
-        :loading="isListLoading"
-        standout
-        dense
-        debounce="300"
-        stack-label
-        dark
-      >
-        <template #prepend>
-          <q-icon :name="iconSearch" />
-        </template>
-        <template #after>
-          <subscribe-button :origin-code="originCode" />
-        </template>
-      </q-input>
+        :is-loading="isListLoading"
+        :origin-code="originCode"
+      />
 
       <destination-group
         class="q-mt-xs"
@@ -43,14 +31,13 @@
 <style lang="scss" module></style>
 
 <script lang="ts">
-import { matSearch as iconSearch } from '@quasar/extras/material-icons'
 import { defineComponent, toRef, watch } from '@vue/composition-api'
 import { Portal } from 'portal-vue'
 
 import TheBreadcrumbs from '@/front/src/layouts/components/the-header/the-breadcrumbs.vue'
 import DestinationGroup from '@/front/src/pages/country/components/destination-group.vue'
+import InputFilter from '@/front/src/pages/country/components/input-filter.vue'
 import SearchTabs from '@/front/src/pages/country/components/search-tabs.vue'
-import SubscribeButton from '@/front/src/pages/country/components/subscribe-button.vue'
 import {
   getBreadcrumbs,
   useGroupedDestinations,
@@ -62,7 +49,7 @@ import { useAggregatedLoader } from '@/shared/src/composables/use-promise-loadin
 
 export default defineComponent({
   components: {
-    SubscribeButton,
+    InputFilter,
     SearchTabs,
     TheBreadcrumbs,
     DestinationGroup,
@@ -101,7 +88,6 @@ export default defineComponent({
         isDestinationListLoading,
       ),
       destinations,
-      iconSearch,
       breadcrumbs: getBreadcrumbs(originCode, isDestinationListLoading),
     }
   },
