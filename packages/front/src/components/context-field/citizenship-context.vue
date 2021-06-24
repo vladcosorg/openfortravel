d70570cda357422e7332db2f00176ee26567c99b
chetzof <vcosvic@gmail.com>
2021-06-24 17:27:04 +0300

    Push

--- packages/front/src/components/context-field/citizenship-context.vue
+++ packages/front/src/components/context-field/citizenship-context.vue
@@ -2,11 +2,16 @@
   <country-dropdown
     v-model="value"
     multiple
-    label="Citizenship"
+    stack-label
+    placeholder="dawdw"
+    :label="`Citizenship ${!value ? '(automatic value)' : ''}`"
     clearable
     v-bind="$attrs"
     bottom-slots
   >
+    <template v-if="!value" #selected
+      ><country-label regular :value="defaultCitizenship"
+    /></template>
     <template #hint>
       <hint>
         Most of the countries apply restrictions based on a citizenship.
@@ -20,16 +25,22 @@ import { computed, defineComponent } from '@vue/composition-api'
 
 import CountryDropdown from '@/front/src/components/context-field/helpers/country-dropdown.vue'
 import Hint from '@/front/src/components/context-field/helpers/hint.vue'
+import CountryLabel from '@/front/src/components/country/country-label.vue'
 import { createComputedSetter } from '@/front/src/pages/guide/guide-composable'
 import { useRootStore } from '@/shared/src/composables/use-plugins'
 import { RestrictionNodeType } from '@/shared/src/restriction-tree/types'
 
 export default defineComponent({
-  components: { Hint, CountryDropdown },
+  components: { CountryLabel, Hint, CountryDropdown },
   inheritAttrs: false,
   props: {},
   setup() {
-    const store = useRootStore()
+    const defaultCitizenship = computed(
+      () =>
+        useRootStore().getters.visitorContextWithDefaults[
+          RestrictionNodeType.ORIGIN
+        ],
+    )
     const setter = createComputedSetter(RestrictionNodeType.CITIZENSHIP)
 
     const value = computed<typeof setter.value>({
@@ -45,7 +56,7 @@ export default defineComponent({
         setter.value = value
       },
     })
-    return { value }
+    return { value, defaultCitizenship }
   },
 })
 </script>
