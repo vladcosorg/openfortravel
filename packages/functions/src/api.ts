375b643dc28fb5fd439687c28c586c1c4e6d4ca5
chetzof <vcosvic@gmail.com>
2021-08-15 00:29:38 +0300

    push

--- packages/functions/src/api.ts
+++ packages/functions/src/api.ts
@@ -1,6 +1,8 @@
+import cors from 'cors'
+import express from 'express'
 import * as functions from 'firebase-functions'
 
-export const api = functions.https.onRequest(async (req, res) => {
-  res.set('Access-Control-Allow-Origin', 'https://openfortravel.org') // don't forget to change this to your domain !
-  res.status(200).send('good')
-})
+const app = express()
+app.use(cors({ origin: true }))
+app.get('/', (_req, res) => res.status(200).send('good'))
+export const api = functions.https.onRequest(app)
